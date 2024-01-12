08696839c864192de9b63e55c29c5562c14ea021
Carlo Roy Briz <cbrizzy003@gmail.com>
2024-01-12 12:53:59 -0600

    connected to mongodb

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri)
+    .then(() => console.log('MongoDB database connection established successfully'))
+    .catch(err => console.log('Error connecting to MongoDB:', err))
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
+
